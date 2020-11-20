77f4020389feb4abf394a51e3df2bb2180889c95
Avery <avery.taylor00@gmail.com>
2020-11-19 21:22:42 -0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,48 @@
-//
-//  main.cpp
-//  heap-min
-//
-//  Created by Avery Taylor  on 11/18/20.
-//  Copyright © 2020 Avery Taylor . All rights reserved.
-//
-
 #include <iostream>
+#include <fstream>
+#define SIZE 10;
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-	return 0;
+using namespace std;
+
+void printLevel(int []);
+void insert(int [],int,int);
+void minCheckAndSwap(int [], int);
+
+int main()
+{
+	ifstream in;
+	int txtFileInput;
+	int index=0;
+	int minHeap[4];
+	in.open
+	("//Users//averytaylor//Desktop//School//cisp430//heap-min//input.txt");
+	
+	while (in >> txtFileInput)
+	{
+		insert(minHeap,txtFileInput,index);
+		index++;
+		cout << txtFileInput << endl;
+	}
+	cout << "helloworld" << endl;
+	return 1;
 }
+
+void insert(int minHeap[], int data, int index)
+{
+	minHeap[index] = data;
+	minCheckAndSwap(minHeap, index);
+}
+
+//this does not work
+void minCheckAndSwap(int minHeap[], int i)
+{
+	while(i>0 &&
+				minHeap[i] > minHeap[(i)/2])
+	{
+		int temp = minHeap[(i)/2];
+		minHeap[(i)/2] = minHeap[i];
+		minHeap[i] = temp;
+		i--;
+	}
+}
+
